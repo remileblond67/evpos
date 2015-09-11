14a72d7c68be39ead8bffc9208e59d08835ab9be
Rémi LEBLOND <remi.leblond@strasbourg.eu>
2015-09-11 13:49:25 +0200

    Intégration des bundles JQuery et Bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new EVPOS\affectationBundle\EVPOSaffectationBundle(),
             new EVPOS\UserBundle\EVPOSUserBundle(),
+            new Evheniy\JqueryBundle\JqueryBundle(),
+            new Anezi\Bundle\BootstrapBundle\BootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
