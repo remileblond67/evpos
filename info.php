856d5e0384aa309127aa70aa377e95e8796f7892
Rémi LEBLOND <remi.leblond@strasbourg.eu>
2015-10-28 16:30:30 +0100

    Correction d'anomalies soulevées par SensioLab Insight

--- info.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo 'Pic : ' . number_format(memory_get_peak_usage(), 0, '.', ',') . " octets\n";
-echo 'Usage : ' . number_format(memory_get_usage(), 0, '.', ',') . " octets\n";
-
-    
-phpinfo();
-
-?>
