7f46c72338849d91bdad8bf56123711e9cf23a58
Rémi LEBLOND <remi.leblond@strasbourg.eu>
2015-12-08 17:56:10 +0100

    Finalisation de la présentation des données.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 # Mise à jour des données de suivi à partir des réfgérentiels
-for cmd in import_baza import_suapp update_avancement import_poste report_util_poste import_acces_appli import_acces_uo import_acces_poste report_acces_poste report_acces_service
+for cmd in import_baza import_suapp update_avancement import_poste report_util_poste import_acces_appli import_acces_uo import_acces_poste report_acces_poste report_acces_service update_avancement
 do
     echo "*********************************************"
     echo "*** Lancement de la commande $cmd "
