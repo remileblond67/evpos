7160e5188f4406b8b91b89fe4ca6ece7f3a615c6
Rémi LEBLOND <remi.leblond@strasbourg.eu>
2015-12-22 12:18:54 +0100

    Ajout d'une fonction de recherche de poste.

--- src/EVPOS/affectationBundle/Controller/PosteController.php
+++ src/EVPOS/affectationBundle/Controller/PosteController.php
@@ -4,76 +4,98 @@ namespace EVPOS\affectationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use EVPOS\affectationBundle\Entity\Poste;
+
 
 class PosteController extends Controller
 {
+
 	/**
-	 * Affichage de la fiche d'un poste
-	 */
+	* Cherche un poste à partir de son hostname
+	*/
+	public function cherchePosteAction() {
+		$request = $this->get('request');
+		$poste = new Poste;
+
+		$form = $this->createFormBuilder($poste)
+		->add('hostname')
+		->getForm();
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			return $this->redirect($this->generateUrl('evpos_fichePoste', array('hostname' => strtoupper(trim($form['hostname']->getData())))));
+		}
+		return $this->render('EVPOSaffectationBundle:Poste:recherche_poste.html.twig', array('form' => $form->createView()));
+	}
+
+	/**
+	* Affichage de la fiche d'un poste
+	*/
 	public function fichePosteAction($hostname) {
 		$poste = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('EVPOSaffectationBundle:Poste')
-			->getPoste($hostname)
+		->getManager()
+		->getRepository('EVPOSaffectationBundle:Poste')
+		->getPoste($hostname)
 		;
-		
-		if ($poste == NULL) {
-            $this->get('request')->getSession()->getFlashBag()->add('erreur', utf8_encode("Impossible de trouver le poste ".$poste));
-            return $this->render('EVPOSaffectationBundle:Default:index.html.twig');
-        }
-        else {
-            return $this->render('EVPOSaffectationBundle:Poste:fiche_poste.html.twig', array('poste' => $poste));
-        }
-	}
-	
-	/**
-     * Liste des postes connus
-     */
-    public function listePosteAction(Request $request) {
-        // Récupération du numéro de page courante
-        $page = $request->query->get('page');
-        
-        if ($page < 1) 
-            $page = 1;
-            
-        $nbParPage = 1000;
-        
-        $listePoste = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('EVPOSaffectationBundle:Poste')
-            ->getPostesPages($page, $nbParPage)
-        ;
-        
-        $nbPages = ceil(count($listePoste)/$nbParPage);
-        if ($nbPages != 0 && $page > $nbPages) {
-            throw $this->createNotFoundException("La page ".$page." n'existe pas.");
-        }
-        return $this->render('EVPOSaffectationBundle:Poste:liste_poste.html.twig',
-                             array('listePoste' => $listePoste,
-                                   'nbPages' => $nbPages,
-                                   'page' => $page));
-    }
 
-    /**
-     * Liste des types de postes à exclure
-     */
-    public function listeTypePosteExclusAction() {
-        // Réparition des postes par type d'usage
-        $nbPosteUsage = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('EVPOSaffectationBundle:Poste')
-            ->getNbPosteUsage()
-        ;
-        
-        // Type de poste exclus
-        $typeExclus = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('EVPOSaffectationBundle:TypeUsageExclus')
-            ->findAll()
-        ;
-        
-        return $this->render('EVPOSaffectationBundle:Poste:liste_type_poste_exclus.html.twig',
-                             array('nbPosteUsage' => $nbPosteUsage,
-                                   'typeExclus' => $typeExclus));
-    }
+		if ($poste === NULL) {
+			$this->get('request')->getSession()->getFlashBag()->add('erreur', utf8_encode("Impossible de trouver le poste ".$hostname));
+			return $this->render('EVPOSaffectationBundle:Default:index.html.twig');
+		}
+		else {
+			return $this->render('EVPOSaffectationBundle:Poste:fiche_poste.html.twig', array('poste' => $poste));
+		}
+	}
+
+	/**
+	* Liste des postes connus
+	*/
+	public function listePosteAction(Request $request) {
+		// Récupération du numéro de page courante
+		$page = $request->query->get('page');
+
+		if ($page < 1)
+		$page = 1;
+
+		$nbParPage = 1000;
+
+		$listePoste = $this->getDoctrine()
+		->getManager()
+		->getRepository('EVPOSaffectationBundle:Poste')
+		->getPostesPages($page, $nbParPage)
+		;
+
+		$nbPages = ceil(count($listePoste)/$nbParPage);
+		if ($nbPages != 0 && $page > $nbPages) {
+			throw $this->createNotFoundException("La page ".$page." n'existe pas.");
+		}
+		return $this->render('EVPOSaffectationBundle:Poste:liste_poste.html.twig',
+		array('listePoste' => $listePoste,
+		'nbPages' => $nbPages,
+		'page' => $page));
+	}
+
+	/**
+	* Liste des types de postes à exclure
+	*/
+	public function listeTypePosteExclusAction() {
+		// Réparition des postes par type d'usage
+		$nbPosteUsage = $this->getDoctrine()
+		->getManager()
+		->getRepository('EVPOSaffectationBundle:Poste')
+		->getNbPosteUsage()
+		;
+
+		// Type de poste exclus
+		$typeExclus = $this->getDoctrine()
+		->getManager()
+		->getRepository('EVPOSaffectationBundle:TypeUsageExclus')
+		->findAll()
+		;
+
+		return $this->render('EVPOSaffectationBundle:Poste:liste_type_poste_exclus.html.twig',
+		array('nbPosteUsage' => $nbPosteUsage,
+		'typeExclus' => $typeExclus));
+	}
 }
