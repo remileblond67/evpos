4a8c73516c4038c8e5a1722d1982607c85ff66ac
Rémi LEBLOND <remi.leblond@laposte.net>
2016-03-17 08:54:08 +0100

    Utilisation du code S2

--- src/EVPOS/affectationBundle/Controller/UpdateController.php
+++ src/EVPOS/affectationBundle/Controller/UpdateController.php
@@ -2,27 +2,25 @@
 namespace EVPOS\affectationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UpdateController extends Controller
 {
     public function updateAvancementAction($codeService) {
       $service = $this->getDoctrine()
-          ->getManager()
-          ->getRepository('EVPOSaffectationBundle:Service')
-          ->getServiceFiche($codeService)
+        ->getManager()
+        ->getRepository('EVPOSaffectationBundle:Service')
+        ->getServiceFiche($codeService)
       ;
 
       $form = $this->createFormBuilder($service)
         ->add('codeService')
         ->add('numEnsemble')
-        ->add('save', SubmitType::class, array('label' => 'Mettre à jour'))
+        ->add('save', submit, array('label' => 'Mettre à jour'))
         ->getForm()
       ;
 
       return $this->render('EVPOSaffectationBundle:Utilisateur:update_ensemble_service.html.twig', array(
-            'form' => $form->createView(),
+        'form' => $form->createView(),
       ));
     }
 }
