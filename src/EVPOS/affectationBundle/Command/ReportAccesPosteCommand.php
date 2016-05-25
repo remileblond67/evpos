4c5a1f9ffc446887373735f8e29ffb786e090d8b
remileblond67 <leblond.remi@gmail.com>
2016-05-25 23:22:36 +0200

    Suppression de la levée de la limite du temps d'exécution

--- src/EVPOS/affectationBundle/Command/ReportAccesPosteCommand.php
+++ src/EVPOS/affectationBundle/Command/ReportAccesPosteCommand.php
@@ -13,40 +13,39 @@ use EVPOS\affectationBundle\Entity\Uo;
  */
 class ReportAccesPosteCommand extends ContainerAwareCommand
 {
-    protected function configure() {
-        parent::configure();
-        $this
-            ->setName('evpos:report_acces_poste')
-            ->setDescription('Report des accès postes au niveau des utilisateurs')
-        ;
-    }
+  protected function configure() {
+      parent::configure();
+      $this
+          ->setName('evpos:report_acces_poste')
+          ->setDescription('Report des accès postes au niveau des utilisateurs')
+      ;
+  }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        ini_set('memory_limit', -1);
-		$em = $this->getContainer()->get('doctrine')->getManager();
+  protected function execute(InputInterface $input, OutputInterface $output) {
+  	$em = $this->getContainer()->get('doctrine')->getManager();
 
-		$output->writeln("*** Report des accès postes sur les utilisateurs ***");
+  	$output->writeln("*** Report des accès postes sur les utilisateurs ***");
     $listePosteUtilisateursAppli = $em->getRepository('EVPOSaffectationBundle:Poste')->getPosteUtilisateursAppli();
 
     foreach($listePosteUtilisateursAppli as $poste) {
-        foreach ($poste->getListeUtilisateurs() as $util) {
-            foreach ($poste->getListeUo() as $uo) {
-              $acces = $em->getRepository('EVPOSaffectationBundle:AccesUtilUo')->getAccesUtilUo($util, $uo);
-              if ($acces === null) {
-    						$acces = new AccesUtilUo();
-    						$acces->setUtilAcces($util);
-    						$acces->setUoAcces($uo);
-    						$acces->setSourceImport("Report depuis accès poste");
-    						$em->persist($acces);
-    						$em->flush();
-    						$output->write("a");
-              } else {
-    						$acces->setSourceImport("Report depuis accès poste");
-    						$em->persist($acces);
-    						$output->write("m");
-    					}
-            }
-        }
+      foreach ($poste->getListeUtilisateurs() as $util) {
+          foreach ($poste->getListeUo() as $uo) {
+            $acces = $em->getRepository('EVPOSaffectationBundle:AccesUtilUo')->getAccesUtilUo($util, $uo);
+            if ($acces === null) {
+  						$acces = new AccesUtilUo();
+  						$acces->setUtilAcces($util);
+  						$acces->setUoAcces($uo);
+  						$acces->setSourceImport("Report depuis accès poste");
+  						$em->persist($acces);
+  						$em->flush();
+  						$output->write("a");
+            } else {
+  						$acces->setSourceImport("Report depuis accès poste");
+  						$em->persist($acces);
+  						$output->write("m");
+  					}
+          }
+      }
     }
     $em->flush();
     $output->writeln("OK");
