1450d0d4645eca3f0497b07ffd21cd146d857e4f
Rémi LEBLOND <remi.leblond@laposte.net>
2016-07-08 15:45:57 +0200

    Mise à jour du code direction du service.

--- src/EVPOS/affectationBundle/Command/ImportBazaCommand.php
+++ src/EVPOS/affectationBundle/Command/ImportBazaCommand.php
@@ -112,6 +112,7 @@ class ImportBazaCommand extends ContainerAwareCommand
         $service->setDirection($em->getRepository('EVPOSaffectationBundle:Direction')->getDirection($codeDirection));
       }
       $service->setCodeService($codeService);
+      $service->setCodeDirection($codeDirection);
       $service->setLibService($libService);
       $service->setExisteBaza(TRUE);
 
