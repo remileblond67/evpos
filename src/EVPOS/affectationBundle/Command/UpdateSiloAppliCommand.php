95ea8158442f64d08a7a5164ef3a485eca68e074
Rémi LEBLOND <remi.leblond@laposte.net>
2016-07-12 18:03:00 +0200

    Création de la commande d'import du fichier XML

---
+++ src/EVPOS/affectationBundle/Command/UpdateSiloAppliCommand.php
@@ -0,0 +1,41 @@
+<?php
+namespace EVPOS\affectationBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+* Import des silos et de l'affectation des UO dans ces derniers
+*/
+class UpdateSiloAppliCommand extends ContainerAwareCommand
+{
+  protected function configure() {
+    parent::configure();
+    $this
+      ->setName('evpos:update_silo_appli')
+      ->setDescription("Mise à jour des silos et de l'affectation des UO dans ces derniers")
+    ;
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    // Identification de l'environnement courant
+    if (strpos(getcwd(), "prod") !== false) {
+      $env = "prod";
+    } else {
+      $env = "dev";
+    }
+
+    $output->writeln("Mise à jour des silos et de l''affectation des UO dans ces derniers (".$env.")");
+
+    $fileName = "/home/data/evpos/".$env."/silo/Update_AppV.xml";
+    try {
+      $xmlFile = fopen($fileName);
+    } catch(Exception $e) {
+      $output->writeln("Impossible d'exploiter le fichier $fileName : ",  $e->getMessage(), "\n");
+    } finaly {
+      fclose($xmlFile);
+    }
+    $output->writeln("OK");
+  }
+}
