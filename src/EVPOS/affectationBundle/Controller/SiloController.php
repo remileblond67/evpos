4677775569c3f1ca9cb73e64f420317b70e4c08d
Rémi LEBLOND <remi.leblond@laposte.net>
2016-07-18 18:30:34 +0200

    Création de la page de liste des silos

---
+++ src/EVPOS/affectationBundle/Controller/SiloController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EVPOS\affectationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use EVPOS\affectationBundle\Entity\Silo;
+
+class ApplicationController extends Controller
+{
+    /**
+     * Liste des silos
+     */
+    public function listeSilo() {
+      $listeSilo = $this->getDoctrine()
+          ->getManager()
+          ->getRepository('EVPOSaffectationBundle:Silo')
+          ->findAll()
+      ;
+      return $this->render('EVPOSaffectationBundle:Silo:liste_silo.html.twig', array('listeSilo' => $listeSilo));
+    }
+}
