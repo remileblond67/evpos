9d323930a4c31b0e97f811cad0e811b4ce0711fe
Rémi LEBLOND <remi.leblond@laposte.net>
2016-07-18 13:49:07 +0200

    Debug

--- src/EVPOS/affectationBundle/Entity/UO.php
+++ src/EVPOS/affectationBundle/Entity/UO.php
@@ -89,7 +89,7 @@ class UO
      * @ORM\JoinTable(name="evpos_silo_uo",
      *   joinColumns={@ORM\JoinColumn(name="code_uo", referencedColumnName="codeUo")},
      *   inverseJoinColumns={@ORM\JoinColumn(name="idSilo", referencedColumnName="id")}
-     )
+     * )
      */
     private $listeSilo;
 
