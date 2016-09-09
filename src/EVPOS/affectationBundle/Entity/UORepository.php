2e8edd4875496f9093fc14a426321f94642174dc
Rémi LEBLOND <remi.leblond@laposte.net>
2016-09-09 16:56:51 +0200

    Mise à jour de la liste des silos

--- src/EVPOS/affectationBundle/Entity/UORepository.php
+++ src/EVPOS/affectationBundle/Entity/UORepository.php
@@ -151,8 +151,11 @@ class UORepository extends EntityRepository
       foreach ($uo->getListeSilo() as $silo) {
         if (in_array($silo->getNomSilo(), $listeSilo)) {
           $listeUoKo[] = $uo->getCodeUo();
+        } else {
+          $listeSilo[] = $silo->getNomSilo();
         }
       }
+      unset($listeSilo);
     }
     return $listeUoKo;
   }
